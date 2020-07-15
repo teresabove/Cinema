8c32ec96e33d9806686227f0604f30a7a600f197
teresabove <teresa.4us@gmail.com>
2020-07-15 18:14:55 +0200

    Risolto mappa

--- cinemaapp/src/app/sala/sala.component.ts
+++ cinemaapp/src/app/sala/sala.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { sala } from '../Models/sala.model'; 
 import { mappa } from '../Models/mappa.model'; 
@@ -18,10 +18,9 @@ export class SalaComponent implements OnInit {
   public posti : Array<posto> = new Array();
   public count: Array<posto> = new Array();
   public proiezione: proiezione = new proiezione();
-  public r: number = 0;
-  public c: number = 0;
-  public p: number = 0;
-
+  //public file: Array<posto>[] = new Array();
+  public r: number;
+  public c: number;
   constructor(public sApi: ApiService, public router: Router) { }
 
   
@@ -32,23 +31,23 @@ export class SalaComponent implements OnInit {
     this.sApi.getSalaattribute(this.proiezione.sala).subscribe(res => {
           this.sala=res;
           console.log(this.sala);
-            this.r = this.sala.numerofile;
-            this.p = this.sala.numeroposti;
-            this.c = this.p/ this.r;
-            for (let i=0; i<this.r; i++){
-              for (let j=0; j<this.c; j++){
-                var p= new posto();
-                p.fila=i;
-                p.numero=j;
-                this.mappa.matrice.push(p);  
-              }
-            }
-            console.log(this.mappa.matrice);
-       });
-    }
-                    
+          this.r = this.sala.numerofile; 
+          this.c = this.sala.numeroposti/this.r;         
+           for (let i=0; i< this.r; i++){
+              this.mappa.matrice[i]=[];
+              for (let j=0; j< this.c; j++){
+               var p = new posto();
+                 p.fila= i;
+                 p.numero = j;
+             this.mappa.matrice[i][j]=p;              
+             } 
+           }
+           console.log(this.mappa.matrice);
+         });
+            
+          }
+          
          
-                  
   
   Seleziona(posto){
       console.log(posto);
@@ -61,6 +60,6 @@ export class SalaComponent implements OnInit {
       this.router.navigate(['/biglietto']);
   }
   
+  }
   
-}
 
